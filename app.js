b82ed343ab9f40e904c5f0f49e5916439a2ec25f
LangChuan-Huang <er230059@gmail.com>
2016-08-16 15:34:55 +0800

    實作前端sensor封包解析IR感測狀態、計算速度 建立網頁伺服器

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.Server(app);
+var io = require('socket.io')(server);
 var i2c = require('i2c');
 var SerialPort = require('serialport');
+
+var IR = [0, 0, 0, 0];
+var speedFeedback = 0;
 
 var speedValue = 0;
 var speedDAC = new i2c(0x62, {device: '/dev/i2c-1'});
@@ -41,6 +49,34 @@ sensorPort.on('error', function(err) {
 	console.log('Sensor module error: ' + err);
 })
 
+var sensorBuffer = [];
 sensorPort.on('data', function (data) {
-	console.log('Sensor module data: ', data);
+	for(var i = 0; i < data.length; i++) {
+		sensorBuffer.push(data[i]);
+	}
+	var startPos = -1;
+	for(var i = 0; i < sensorBuffer.length - 1; i++) {
+		if(sensorBuffer[i] == 0x01 && sensorBuffer[i + 1] == 0x02) {
+			startPos = i + 2;
+		}
+	}
+	if(startPos != -1 && (sensorBuffer.length - startPos) >= 3) {
+		var buf = new Buffer([sensorBuffer[startPos], sensorBuffer[startPos + 1]]);
+		speedFeedback = Math.round(buf.readUInt16BE(0) * 0.03685 * 100) / 100;
+		var bIR = ('000' + sensorBuffer[startPos + 2].toString(2)).slice(-4);
+		IR = bIR.split('').reverse();
+		sensorBuffer.splice(startPos - 2, 5);
+	}
+	console.log("IR: " + IR + " Speed: " + speedFeedback);
+});
+
+app.use(express.static(__dirname));
+
+app.use(function (req, res, next) {
+	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	console.log(ip + ': '+ req.url);
+	next();
+});
+
+server.listen(80, function () {
 });
