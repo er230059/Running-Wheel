0974f294020052ef76bd812fc048d7462b35935e
LangChuan-Huang <er230059@gmail.com>
2016-08-18 12:48:39 +0800

    更新PPG sensor取樣速度為100Hz (使用低速啟動指令封包) 與PPG sensor通訊前先flush IO 完成PPG sensor封包解析

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var io = require('socket.io')(server);
 var i2c = require('i2c');
 var SerialPort = require('serialport');
 
+var ppgData;
 var IR = [0, 0, 0, 0];
 var speedFeedback = 0;
 
@@ -27,10 +28,15 @@ var sensorPort = new SerialPort('/dev/ttyO4', {
 });
 
 ppgPort.on('open', function() {
-	ppgPort.write([0x30, 0x20, 0x05, 0x0A, 0x01, 0x01, 0x31], function(err) {
+	ppgPort.flush(function(err) {
 		if (err) {
-			console.log('PPG write wrror: ', err);
+			console.log('PPG flush wrror: ', err);
 		}
+		ppgPort.write([0x30, 0x20, 0x05, 0x64, 0x01, 0x01, 0x8B], function(err) {
+			if (err) {
+				console.log('PPG write wrror: ', err);
+			}
+		});
 	});
 });
 
@@ -38,8 +44,37 @@ ppgPort.on('error', function(err) {
 	console.log('PPG error: ' + err);
 })
 
+var ppgBuffer = [];
 ppgPort.on('data', function (data) {
-	console.log('PPG data: ', data);
+	for(var i = 0; i < data.length; i++) {
+		ppgBuffer.push(data[i]);
+	}
+	var startPos = -1;
+	for(var i = 0; i < ppgBuffer.length - 2; i++) {
+		if(ppgBuffer[i] == 0x30 && ppgBuffer[i + 1] == 0x20 && ppgBuffer[i + 2] == 0x13) {
+			startPos = i;
+			break;
+		}
+	}
+	if(startPos != -1 && (ppgBuffer.length - startPos) >= 21) {
+		var xAxisBuf = new Buffer([ppgBuffer[startPos + 8], ppgBuffer[startPos + 9]]);
+		var yAxisBuf = new Buffer([ppgBuffer[startPos + 10], ppgBuffer[startPos + 11]]);
+		var zAxisBuf = new Buffer([ppgBuffer[startPos + 12], ppgBuffer[startPos + 13]]);
+		var ppgBuf = new Buffer([ppgBuffer[startPos + 14], ppgBuffer[startPos + 15]]);
+		var ppg10Buf = new Buffer([ppgBuffer[startPos + 16], ppgBuffer[startPos + 17]]);
+		var ppg100Buf = new Buffer([ppgBuffer[startPos + 18], ppgBuffer[startPos + 19]]);
+		ppgData = {
+			'sn': ppgBuffer[startPos + 3],
+			'x': xAxisBuf.readUInt16BE(0),
+			'y': yAxisBuf.readUInt16BE(0),
+			'z': zAxisBuf.readUInt16BE(0),
+			'ppg': ppgBuf.readUInt16BE(0),
+			'ppgx10': ppg10Buf.readUInt16BE(0),
+			'ppgx100': ppg100Buf.readUInt16BE(0)
+		};
+		ppgBuffer.splice(0, startPos + 21);
+		console.log('PPG data: ' + JSON.stringify(ppgData));
+	}
 });
 
 sensorPort.on('open', function() {
@@ -57,17 +92,18 @@ sensorPort.on('data', function (data) {
 	var startPos = -1;
 	for(var i = 0; i < sensorBuffer.length - 1; i++) {
 		if(sensorBuffer[i] == 0x01 && sensorBuffer[i + 1] == 0x02) {
-			startPos = i + 2;
+			startPos = i;
+			break;
 		}
 	}
-	if(startPos != -1 && (sensorBuffer.length - startPos) >= 3) {
-		var buf = new Buffer([sensorBuffer[startPos], sensorBuffer[startPos + 1]]);
+	if(startPos != -1 && (sensorBuffer.length - startPos) >= 5) {
+		var buf = new Buffer([sensorBuffer[startPos + 2], sensorBuffer[startPos + 3]]);
 		speedFeedback = Math.round(buf.readUInt16BE(0) * 0.03685 * 100) / 100;
-		var bIR = ('000' + sensorBuffer[startPos + 2].toString(2)).slice(-4);
+		var bIR = ('000' + sensorBuffer[startPos + 4].toString(2)).slice(-4);
 		IR = bIR.split('').reverse();
-		sensorBuffer.splice(startPos - 2, 5);
+		sensorBuffer.splice(0,startPos + 5);
+		console.log('IR: ' + IR + ' Speed: ' + speedFeedback);
 	}
-	console.log("IR: " + IR + " Speed: " + speedFeedback);
 });
 
 app.use(express.static(__dirname));
